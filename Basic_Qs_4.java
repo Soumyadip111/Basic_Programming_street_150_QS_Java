c116106aca6effe1bf20d8af1b3f5fed5e69e355
Soumyadip Giri <159867738+Soumyadip111@users.noreply.github.com>
2025-02-23 10:36:44 +0530

    Add files via upload
    4.	Calculating Armstrong Numbers
    Difficulty: Easy
    Topics: Basic Programming, Number Theory
    Description: Write a program to check if a number is an Armstrong number.
    Example:
    Input: number = 153
    Output: Armstrong Number
    Explanation: 153 is an Armstrong number because 1^3 + 5^3 + 3^3 = 153.

---
+++ Basic_Qs_4.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Basic_Qs_4 {
+
+    public static boolean CheckArmstrong(int num) {
+        int SameNum = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length(); // length of number (153 = length 3)
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digits, digit);
+            num /= 10; // remove last digit
+        }
+        return sum == SameNum; // sum becomes 0 to 153 after iteration then SameNum is same equal to num
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number as an input : ");
+        int num = sc.nextInt();
+
+        if (CheckArmstrong(num)) {
+            System.out.println(num + " is Armstrong number");
+        } else {
+            System.out.println(num + " is not Armstrong number");
+        }
+    }
+}
