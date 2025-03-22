397a83fbb4eef087b2f9dd184755f93dc807784c
Soumyadip Giri <159867738+Soumyadip111@users.noreply.github.com>
2025-03-22 15:18:11 +0530

    Update Basic_Qs_7.java

--- Basic_Qs_7.java
+++ Basic_Qs_7.java
@@ -4,7 +4,7 @@ public class Basic_Qs_7 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print(" Enter your Pyramid Height ");
+        System.out.print(" Enter height of the Pyramid");
         int h = sc.nextInt(); // Here 'h' use for here Height means number of rows
         for (int i = 1; i <= h; i++) {
             // Print spaces 
