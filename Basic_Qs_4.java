c6eb08a53223369744b267628160b1db9937a51e
Soumyadip Giri <159867738+Soumyadip111@users.noreply.github.com>
2025-03-23 21:04:24 +0530

    Update Basic_Qs_4.java

--- Basic_Qs_4.java
+++ Basic_Qs_4.java
@@ -17,7 +17,7 @@ public class Basic_Qs_4 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number as an input : ");
+        System.out.print("Enter a number : ");
         int num = sc.nextInt();
 
         if (CheckArmstrong(num)) {
