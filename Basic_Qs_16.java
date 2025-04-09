c80390c552ff80ffdcdc4f6cad97547b1804066d
Soumyadip Giri <159867738+Soumyadip111@users.noreply.github.com>
2025-04-09 16:43:12 +0530

    Add files via upload
    16.	Finding the Sum of Elements in an Array
    Difficulty: Easy
    Topics: Basic Programming, Arrays
    Description: Write a program to find the sum of elements in an array.
    Example:
    Input: array = [1, 2, 3, 4, 5]
    Output: 15
    Explanation: The sum of the elements in the array is 15.

---
+++ Basic_Qs_16.java
@@ -0,0 +1,12 @@
+
+public class Basic_Qs_16 {
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        int sum = 0;  // here i use  sum = 0 for if we add with 0 + 1 we will get  1 
+        for (int i = 0; i < arr.length; i++) {  
+            sum += arr[i];
+        }
+        System.out.println("Sum is " + sum);
+    }
+}
