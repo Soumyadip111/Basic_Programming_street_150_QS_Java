da511339987f68a78c3cd8fa2041303e2b7edf6e
Soumyadip Giri <159867738+Soumyadip111@users.noreply.github.com>
2025-04-22 08:13:52 +0530

    Update Basic_Qs_16.java

--- Basic_Qs_16.java
+++ Basic_Qs_16.java
@@ -5,7 +5,7 @@ public class Basic_Qs_16 {
         int arr[] = {1, 2, 3, 4, 5};
         int sum = 0;  // here i use  sum = 0 for if we add with 0 + 1 we will get  1 
         for (int i = 0; i < arr.length; i++) {  
-            sum += arr[i];
+            sum += arr[i];  // first iteration 0+1 to arr.length
         }
         System.out.println("Sum is " + sum);
     }
