4a415cc093139b01a9ad5816441c0519e7d26196
Soumyadip Giri <159867738+Soumyadip111@users.noreply.github.com>
2025-07-08 11:03:00 +0530

    Add files via upload

---
+++ Basic_Qs_21.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Basic_Qs_21 {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter  a num ");
+      int n = sc.nextInt();
+      int sum = 0;
+      for(int i = 1; i<=10; i++){
+         if(i % 2 == 0){
+            sum += i;
+         }
+      }
+      System.out.println("Sum of All Even Numbers : " + sum);
+   }
+}
