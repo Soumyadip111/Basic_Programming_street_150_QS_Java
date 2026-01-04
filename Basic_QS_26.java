cd77ea55fea2e458123956754b8661261c9e25e2
Soumyadip Giri <159867738+Soumyadip111@users.noreply.github.com>
2026-01-04 10:16:01 +0530

    Create Basic_QS_26.java

---
+++ Basic_QS_26.java
@@ -0,0 +1,10 @@
+class Factors{
+    public static void main(String[] args) {
+        int a = 20;
+        for(int i =1; i<=a; i++){
+            if( a % i ==0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
