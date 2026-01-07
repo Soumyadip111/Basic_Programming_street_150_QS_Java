81923bb91ceb18d37165e4f8a723c2539cf33f85
Soumyadip Giri <159867738+Soumyadip111@users.noreply.github.com>
2026-01-07 09:44:59 +0530

    Create Basic_qs_29.java

---
+++ Basic_qs_29.java
@@ -0,0 +1,17 @@
+class AP {
+    public static void main(String[] args) {
+            int a = 2;
+            int d = 3;
+            int n = 15;
+
+
+            int term = a;
+
+
+            for(int i =1; i<=n; i++){
+                System.out.print(term+" ");
+                 term = term + d;
+            }
+    }
+}
+
