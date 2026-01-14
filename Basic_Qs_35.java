2b0c525918bddeae7a04c720de0232147dc3b19c
Soumyadip Giri <159867738+Soumyadip111@users.noreply.github.com>
2026-01-14 21:40:11 +0530

    Create Basic_Qs_35.java

---
+++ Basic_Qs_35.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args)  {
+       int n =5;
+       for(int i =1; i<=n; i++)
+       {
+        for(int j = 0; j<n-i;j++)
+        {
+            System.out.print(" ");
+        }
+        for(int k= 0; k<i*2-1;k++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+       }
+    }
+}
+// output
+//     *
+//    ***
+//   *****
+//  *******
+// *********
