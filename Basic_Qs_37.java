fe6ffd02656d3e41b19d01a572ca4170fe9c559a
Soumyadip Giri <159867738+Soumyadip111@users.noreply.github.com>
2026-01-17 10:37:24 +0530

    Create Basic_Qs_37.java

---
+++ Basic_Qs_37.java
@@ -0,0 +1,13 @@
+// Print Numbers in an Increasing Sequence (1, 12, 123, 1234, 12345)
+
+class Pattern{
+    public static void main(String[] args) {
+      int n =5;
+       for(int i=1; i<=n;i++){
+        for(int j =1; j<=i; j++){
+            System.out.print(j);
+        }
+        System.out.println();
+       }
+    }
+}
