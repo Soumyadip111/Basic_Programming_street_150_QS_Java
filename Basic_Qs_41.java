4dd7a1187ecd60797f3a7ddfe924ad7063ccf64e
Soumyadip Giri <159867738+Soumyadip111@users.noreply.github.com>
2026-01-21 10:40:28 +0530

    Create Basic_Qs_41.java

---
+++ Basic_Qs_41.java
@@ -0,0 +1,23 @@
+
+class Pattern {
+    public static void main(String[] args) {
+        int row =7;
+        int n =1;
+        for(int i =1; i<=row;  i++)
+        {
+            for(int j=1; j<=i; j++){
+                System.out.print(n % 10 +" ");
+                n++;
+            }
+                System.out.println();
+        }
+    }
+}
+// output
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 0
+// 1 2 3 4 5
+// 6 7 8 9 0 1
+// 2 3 4 5 6 7 8
