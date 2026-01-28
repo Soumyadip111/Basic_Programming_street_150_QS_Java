2f49fd7c0b8be70cf46dcf4676ffbae645a77500
Soumyadip Giri <159867738+Soumyadip111@users.noreply.github.com>
2026-01-28 15:36:53 +0530

    Create Basic_Qs_47.java

---
+++ Basic_Qs_47.java
@@ -0,0 +1,20 @@
+
+class Pattern {
+    public static void main(String[] args) {
+        int n =5;
+        for(int i = 0; i<n; i++){
+              char ch = (char) ('A' +i);
+            for(int j =0; j<=i; j++){
+                 System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// output
+// A
+// B B
+// C C C
+// D D D D
+// E E E E E
